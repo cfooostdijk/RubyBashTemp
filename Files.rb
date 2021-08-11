23c4479eb923c5b9f4b8e8cc93033d995d8de18a
Cristian <cfo.oostdijk@gmail.com>
2021-08-10 23:00:30 -0300

    File update

--- Files.rb
+++ Files.rb
@@ -1,30 +1,25 @@
-require './Users.rb'
 require 'tempfile'
 require 'etc'
 
 PATH = Dir.pwd
 
 class Files
-
+  
   def create_file(name, current_user) # Only Super and Regular Users
     return puts "File exist" if File.exist?("#{name}.txt")
+    return puts "Not authorized" if current_user[2] == 'Read_only'
     if current_user[2] != 'Read_only'
       puts "Persist files? (yes or no)" 
       text = gets.chomp
+      return puts "It's a Yes or No question" unless text != "yes" || text != "no"
       puts "Write file content"
       content = gets.chomp
       if text == "no"
-        file = Tempfile.new("#{name}.txt", PATH)
-        file.write("#{content}")
-        file.rewind
-      elsif text == "yes"
-        File.open("#{name}.txt", "w") { |f| f << "#{content}" }   
+        self.ftemp(name, content)
       else
-        puts "It's a Yes or No question"
+        File.open("#{name}.txt", "w") { |f| f << "#{content}" }   
       end
-    else
-      puts 'Not authorized'
-    end 
+    end
   end
 
   def show(name) # Public
@@ -54,5 +49,11 @@ class Files
 
   def help # Public
     File.foreach("readme.txt") { |line| puts line }
+  end
+
+  def ftemp(name, content)
+    file = Tempfile.new("#{name}.txt", PATH)
+    file.write("#{content}")
+    file.rewind
   end
 end
